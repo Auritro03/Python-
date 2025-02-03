528dbe077ef454a088a7540f981d6e20eeb3f6c7
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2025-02-03 21:49:24 +0600

    tuple done

---
+++ tuple.py
@@ -0,0 +1,5 @@
+tp = (["POP JEE", 20002], ["SOP JEE", 20003], ["DOP JEE", 20004])
+tp[0][1] = 0
+print(tp)
+# can't declare single element in tuple. Must need to use a comma (,) after the element if it is single element.
+
