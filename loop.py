99945ffb878dc16e885c89fea0e316b6594b2516
Easa Yeasfi Bin Islam <oritrobinislam@gmail.com>
2025-03-05 23:22:28 +0600

    loop done

---
+++ loop.py
@@ -0,0 +1,9 @@
+myList = [{'Oritro', '23', 'Mirpur'}, {'AAA', '23', 'Uttara'}]
+# for a, b, c in myList:
+#     print(a, b, c)
+myDict = {'Name': 'Oritro', 'Home': 'Dhaka', 'Region': 'Asia'}
+# for a, b in myDict.items():
+#     print(f"{a} = {b}")
+myStr = 'Oritro'
+for a in myStr:
+    print(a)
